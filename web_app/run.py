4d9e62f6564964dad84bac34e7ddd90d9713a748
st18181ys <st18181ys@gm.ibaraki-ct.ac.jp>
2022-07-01 15:18:35 +0900

    Add explanation
    説明追加

--- web_app/run.py
+++ web_app/run.py
@@ -1,10 +1,15 @@
+#　南義貴
+
+# モジュールのインポート
 from flask import Flask,render_template,request
 app=Flask(__name__)
 
+#　最初の表示
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#　変更後の表示
 @app.route('/',methods=['post'])
 def post():
     name=request.form['name']
