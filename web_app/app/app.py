ee78bfea29dbef1cb9bba44c10d12804309be70b
st18181ys <st18181ys@gm.ibaraki-ct.ac.jp>
2022-07-05 20:06:39 +0900

    app.py修正

--- web_app/app/app.py
+++ web_app/app/app.py
@@ -1,7 +1,6 @@
-#　南義貴
-
 # モジュールとファイルのインポート
 from flask import Flask,render_template,request
+import cv2
 import GenerateTextImage
 
 app=Flask(__name__)
@@ -12,23 +11,52 @@ def index():
     return render_template('index.html')
 
 #　変更後の表示
-#　入力した情報を保持して画像を出力
 @app.route('/index',methods=["post"])
 def post():
+
+    # 入力したデータを取得
     text=request.form['text']
     font=request.form['font']
     color=request.form['color']
-    charsize=request.form['charsize']
-    imgsize=request.form['imgsize']
+    textsize=request.form['textsize']
+    height=request.form['height']
+    width=request.form['width']
 
-    generate=generate()
-    backcolor=(imgsize,imgsize,3)
+    # インスタンス生成
+    generate=GenerateTextImage.GenerateTextImage()
+
+    # 背景色を白にする（白はRGB=(255,255,255)）
+    backcolor=(255,255,255)
+
+    # フォントの確認
+    # OpenCVのフォント指定を辞書型で定義
+    fonttype={
+        "simplex":cv2.FONT_HERSHEY_SIMPLEX,
+        "plain":cv2.FONT_HERSHEY_PLAIN,
+        "duplex":cv2.FONT_HERSHEY_DUPLEX,
+        "complex":cv2.FONT_HERSHEY_COMPLEX,
+        "triplex":cv2.FONT_HERSHEY_TRIPLEX,
+        "complex_small":cv2.FONT_HERSHEY_COMPLEX_SMALL,
+        "script_simplex":cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
+        "script_complex":cv2.FONT_HERSHEY_SCRIPT_COMPLEX,
+        "italic":cv2.FONT_ITALIC
+    }
+    # フォントを指定
+    for i in fonttype.keys():
+        if i == font:
+            font=fonttype[i]
+    
+    # テキストの色を指定 
+    color=(int(color[1:3],16),int(color[3:5],16),int(color[5:7],16))
+
+    # 画像を生成
     generate.generate(
-        generate,imgsize,imgsize,backcolor,text,color,font,charsize)
+        int(height),int(width),backcolor,text,color,font,int(textsize))
 
+    # HTMLを返す
     return render_template(
         'index.html',text=text,font=font,color=color,
-        charsize=charsize,imgsize=imgsize)
+        textsize=textsize,height=height,width=width)
 
 if __name__=='__main__':
     app.run(debug=True)
